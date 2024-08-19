4f82a8bcb1e7d80e8e67c17d156752687d94798e
ejsong25 <ejsong25@gmail.com>
2024-08-20 00:37:23 +0900

    msa eureka server, client

---
+++ discoverysevice/src/test/java/com/inflearn/discoverysevice/DiscoveryseviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.discoverysevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryseviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
