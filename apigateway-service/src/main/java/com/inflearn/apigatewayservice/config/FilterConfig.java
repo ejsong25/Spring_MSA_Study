0ed1c255d31357ff631e5540bd6003dbd8a4200e
ejsong25 <ejsong25@gmail.com>
2024-08-27 23:29:17 +0900

    feat: Spring cloud gateway service

---
+++ apigateway-service/src/main/java/com/inflearn/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.inflearn.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class FilterConfig {
+//    @Bean
+    public RouteLocator gqtewayRoutes(RouteLocatorBuilder builer){
+        return builer.routes()
+//                route 정보 설정
+                .route(r-> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+                                       .addResponseHeader("first-response", "first-response-header"))
+                        .uri("http://localhost:8001"))
+                .route(r-> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-response", "second-response-header"))
+                        .uri("http://localhost:8002"))
+                .build();
+    }
+}
